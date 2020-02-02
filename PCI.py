d7a9d70520e14e2539f6f5a72d269d5d17712a43
Luís Eduardo Pompeu <56649205+luiseduardobr1@users.noreply.github.com>
2020-02-02 16:52:26 -0300

    Update PCI.py

--- PCI.py
+++ PCI.py
@@ -18,7 +18,7 @@ soup = BeautifulSoup(data, "html.parser") # complete html source code
 
 # ---------- CONCURSOS NACIONAIS --------------------------------
 source_nacional=str(soup)
-nacional=source_nacional[source_nacional.find('<h2>NACIONAL</h2>')+len('<h2>NACIONAL</h2>'):source_nacional.find('<h2>REGI√O SUDESTE</h2>')+len('<h2>REGI√O SUDESTE</h2>')]
+nacional=source_nacional[source_nacional.find('<h2>NACIONAL</h2>')+len('<h2>NACIONAL</h2>'):source_nacional.find('<h2>REGI√ÉO SUDESTE</h2>')+len('<h2>REGI√ÉO SUDESTE</h2>')]
 extract_nacional=BeautifulSoup(nacional,"html.parser")
 
 name=[]
@@ -31,28 +31,29 @@ combinacao_nacional=[]
 for line in extract_nacional.findAll(class_='ca'):
     name.append(line.find('a').text.strip()) # get name of institution
     vagas.append(re.findall('(\d*) vaga', str(line.find(class_='cd')))[0]) # get number of available job's
-    nivel.append('/'.join(re.findall('Superior|MÈdio', str(line.find(class_='cd'))))) # level
+    nivel.append('/'.join(re.findall('Superior|M√©dio', str(line.find(class_='cd'))))) # level
     salario.append(''.join(re.findall('R\$ *\d*\.*\d*\,*\d*', str(line.find(class_='cd'))))) # salary
     inscricao.append(''.join(re.findall('\d+/\d+/\d+', str(line.find(class_='ce')))))
     
-# ---------- CONCURSOS CEAR¡ --------------------------------
-ceara=source_nacional[source_nacional.find('<div class="uf">CEAR¡</div>')+len('<div class="uf">CEAR¡</div>'):source_nacional.find('<div class="uf">MARANH√O</div>')+len('<div class="uf">MARANH√O</div>')]
+# ---------- CONCURSOS CEAR√Å --------------------------------
+ceara=source_nacional[source_nacional.find('<div class="uf">CEAR√Å</div>')+len('<div class="uf">CEAR√Å</div>'):source_nacional.find('<div class="uf">MARANH√ÉO</div>')+len('<div class="uf">MARANH√ÉO</div>')]
 extract_ceara=BeautifulSoup(ceara,"html.parser")
 
 for line in extract_ceara.findAll(class_='ca'):
     name.append(line.find('a').text.strip()) # get name of institution
     vagas.append(''.join(re.findall('(\d*) vaga', str(line.find(class_='cd'))))) # get number of available job's
-    nivel.append('/'.join(re.findall('Superior|MÈdio', str(line.find(class_='cd'))))) # level
+    nivel.append('/'.join(re.findall('Superior|M√©dio', str(line.find(class_='cd'))))) # level
     salario.append(''.join(re.findall('R\$ *\d*\.*\d*\,*\d*', str(line.find(class_='cd'))))) # salary
     inscricao.append(''.join(re.findall('\d+/\d+/\d+', str(line.find(class_='ce')))))
 
+# Salvando no arquivo CSV
 for i in range(0,len(name)):
     combinacao_nacional=[name[i],vagas[i],nivel[i],salario[i],inscricao[i]]
     df=pd.DataFrame(combinacao_nacional)
     with open('ConcursosAtivos'+d1+'.csv', 'a', encoding='utf-16', newline='') as f:
         df.transpose().to_csv(f, encoding='iso-8859-1', header=False, sep = "\t")
 
-# Conferindo se h· algum concurso novo        
+# Conferindo se h√° algum concurso novo        
 try:
     tamanho_arquivo_original=os.path.getsize('ConcursosAtivos.csv')
     tamanho_arquivo_temp=os.path.getsize('ConcursosAtivos'+d1+'.csv') 
